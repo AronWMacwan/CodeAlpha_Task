91b0ffee04d3f9fe2f351f2486ac1b7133ad399f
Aron Wilson Macwan <166802094+AronWMacwan@users.noreply.github.com>
2025-08-04 16:01:42 +0530

    Add files via upload

---
+++ codealpha_music-player/js/music-list.js
@@ -0,0 +1,41 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+let allMusic = [
+  {
+    name: "Band4Band",
+    artist: "Central Cee",
+    img: "band4band",
+    src: "band4band",
+  },
+  {
+    name: "Congratulations",
+    artist: "Post Malone",
+    img: "congratulations",
+    src: "congratulations",
+  },
+  {
+    name: "One more light",
+    artist: "Linkin Park",
+    img: "one more light",
+    src: "one more light",
+  },
+  {
+    name: "Sprinter",
+    artist: "Central Cee",
+    img: "sprinter",
+    src: "sprinter",
+  },
+  {
+    name: "Sunflower",
+    artist: "Post Malone",
+    img: "sunflower",
+    src: "sunflower",
+  },
+];
